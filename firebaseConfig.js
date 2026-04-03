c748ec7ba2aa01669600215dd7a36e7142b9f3f9
2030grecko-spec <2030grecko@gmail.com>
2026-04-03 09:26:02 -0300

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+// Configuración de Conexión Vínculo -> Firebase
+export const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "vinculo-93704.firebaseapp.com",
+  databaseURL: "https://vinculo-93704-default-rtdb.firebaseio.com",
+  projectId: "vinculo-93704",
+  storageBucket: "vinculo-93704.appspot.com",
+  messagingSenderId: "TU_ID",
+  appId: "TU_APP_ID"
+};
+
