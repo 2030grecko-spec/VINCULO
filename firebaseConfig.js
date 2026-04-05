e4f77f410a69e586938f4842ff824eec69dffc85
2030grecko-spec <2030grecko@gmail.com>
2026-04-05 13:15:55 -0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,10 +1,10 @@
-// Configuración de Conexión Vínculo -> Firebase
+// Configuración de Conexión Vínculo -> Firebase (ACTUALIZADO)
 export const firebaseConfig = {
-  apiKey: "TU_API_KEY",
+  apiKey: "AIzaSyBlRRooVLA5QjimOsieIQW9C8juTB8GEO0",
   authDomain: "vinculo-93704.firebaseapp.com",
   databaseURL: "https://vinculo-93704-default-rtdb.firebaseio.com",
   projectId: "vinculo-93704",
-  storageBucket: "vinculo-93704.appspot.com",
-  messagingSenderId: "TU_ID",
-  appId: "TU_APP_ID"
+  storageBucket: "vinculo-93704.firebasestorage.app",
+  messagingSenderId: "644260536046",
+  appId: "1:644260536046:web:8087480ad1cfeadd325dff"
 };
