27c83631e71d42e1e115be123d75500ae357c59c
2030grecko-spec <2030grecko@gmail.com>
2026-04-06 12:14:54 -0300

    Delete core/voiceEngine.js

--- core/voiceEngine.js
+++
@@ -1,9 +0,0 @@
-export async function hablar(texto) {
-
-  // 👉 SI USAS ELEVENLABS, CAMBIA AQUÍ
-  const voz = new SpeechSynthesisUtterance(texto);
-  voz.lang = "es-ES";
-  voz.rate = 0.9;
-
-  speechSynthesis.speak(voz);
-}
