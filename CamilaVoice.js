3393f21419af838b9d5442c71dee1fb68e4f09db
2030grecko-spec <2030grecko@gmail.com>
2026-04-06 11:52:05 -0300

    Update and rename CamilaVoice.js to engine/viralEngine.js

--- CamilaVoice.js
+++
@@ -1,7 +0,0 @@
-export function hablar(mensaje) {
-    const voz = new SpeechSynthesisUtterance(mensaje);
-    voz.lang = 'es-ES';
-    voz.rate = 0.9; // Tono calmado de amiga
-    window.speechSynthesis.speak(voz);
-}
-// Ejemplo: hablar("Hola Greco, el video de deporte tiene alto valor. Iniciando impulso.");
